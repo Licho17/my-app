86aad77d7719d97ae3d1c410982865bfa6ae2073
Encarnacion <encarnacionlino8@gmail.com>
2023-02-08 19:41:31 -0600

    Nombre del Proyecto: my-app. Descripcion: se añade un nuevo commit

---
+++ src/main/java/com/licho/myapp/entity/TestEntity.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.licho.myapp.entity;
+
+/**
+ * @author encar
+ *
+ */
+public class TestEntity {
+
+}
+private void getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(void nombre) {
+		this.nombre = nombre;
+		
+		private void nombre;
+	}
+
